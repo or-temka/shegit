8f8ce78f42dfba83ea54c1558641dac69bbd8ba9
artem schegortzov <tema.chegortzov@mail.ru>
2024-10-24 01:05:08 +0300

    Init

--- src/types/color/rgb/index.ts
+++
@@ -1,7 +0,0 @@
-export type RgbArrayComponentValue = number;
-export type RgbArray = [RgbArrayComponentValue?, RgbArrayComponentValue?, RgbArrayComponentValue?];
-export interface RgbObj {
-  r: RgbArrayComponentValue;
-  g: RgbArrayComponentValue;
-  b: RgbArrayComponentValue;
-}
