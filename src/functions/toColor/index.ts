255cbedd8c938d4bf794991c23cfb7e97d4aa5cf
Or-temka <69795850+or-temka@users.noreply.github.com>
2024-11-05 00:53:09 +0300

    Add dependencies for toColor function & some settings (#66)

--- src/functions/toColor/index.ts
+++ src/functions/toColor/index.ts
@@ -1,5 +1,5 @@
 import { RgbaObj } from 'types';
-import { rgbArrayToObj } from '../../index';
+import { rgbArrayToObj } from '@shegit/rgb-array-to-obj';
 import { DEFAULT } from './constants';
 import { ColorProp, ReturnColorType, To } from './types';
 
