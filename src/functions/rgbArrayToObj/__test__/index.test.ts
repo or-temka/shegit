9737d4cc76ffc303f855f57168c37e78df0736eb
artem schegortzov <tema.chegortzov@mail.ru>
2024-11-18 21:57:57 +0300

    Split test for gbArrayToObj on groups file

--- src/functions/rgbArrayToObj/__test__/index.test.ts
+++ src/functions/rgbArrayToObj/__test__/index.test.ts
@@ -1,37 +1,11 @@
 import { rgbArrayToObj } from '../../../../dist/functions/rgbArrayToObj';
+import { describeTestGroups } from '../../../../utils';
+import { TestGroups } from '../../../../utils/test/describeTestGroups/types';
+import { extraElements, invalidTypes, mixedCases, partialArrays, validCases } from './groups';
+import { TestGroupCaseElementExpected, TestGroupCaseElementInput } from './types';
 
 describe('RGB array to object', () => {
-  const validCases = [
-    { id: 1, input: [1, 2, 3], expected: { r: 1, g: 2, b: 3 } },
-    { id: 2, input: [0, 0, 0], expected: { r: 0, g: 0, b: 0 } },
-    { id: 3, input: [255, 255, 255], expected: { r: 255, g: 255, b: 255 } },
-    { id: 4, input: [53, 84, 2], expected: { r: 53, g: 84, b: 2 } },
-  ];
-
-  const partialArrays = [
-    { id: 5, input: [234, 12], expected: { r: 234, g: 12, b: 0 } },
-    { id: 6, input: [85], expected: { r: 85, g: 0, b: 0 } },
-  ];
-
-  const extraElements = [
-    { id: 7, input: [124, 62, 55, 0.5], expected: { r: 124, g: 62, b: 55 } },
-    { id: 8, input: [5, 42, 0, 342, 23], expected: { r: 5, g: 42, b: 0 } },
-  ];
-
-  const invalidTypes = [
-    { id: 13, input: 5345, expected: { r: 0, g: 0, b: 0 } },
-    { id: 14, input: '5876', expected: { r: 0, g: 0, b: 0 } },
-    { id: 15, input: 'John', expected: { r: 0, g: 0, b: 0 } },
-    { id: 16, input: { name: 'John' }, expected: { r: 0, g: 0, b: 0 } },
-    { id: 18, input: () => 4, expected: { r: 0, g: 0, b: 0 } },
-  ];
-
-  const mixedCases = [
-    { id: 20, input: () => [15, -26, '+64'], expected: { r: 15, g: 0, b: 64 } },
-    { id: 23, input: ['--126', false, '+267'], expected: { r: 0, g: 0, b: 255 } },
-  ];
-
-  const testGroups = [
+  const testGroups: TestGroups<TestGroupCaseElementInput, TestGroupCaseElementExpected> = [
     { name: 'Valid cases', cases: validCases },
     { name: 'Partial arrays', cases: partialArrays },
     { name: 'Extra elements', cases: extraElements },
@@ -39,18 +13,8 @@ describe('RGB array to object', () => {
     { name: 'Mixed cases', cases: mixedCases },
   ];
 
-  testGroups.forEach((group) => {
-    describe(group.name, () => {
-      const lastCaseId = group.cases[group.cases.length - 1]?.id;
-
-      group.cases.forEach((test) => {
-        it(`(${test.id}/${lastCaseId}) input: ${test.input}, expected: ${JSON.stringify(
-          test.expected,
-        )}`, () => {
-          const actual = rgbArrayToObj(test.input);
-          expect(actual).toStrictEqual(test.expected);
-        });
-      });
-    });
+  describeTestGroups({
+    testGroups,
+    callback: (input) => rgbArrayToObj(input),
   });
 });
