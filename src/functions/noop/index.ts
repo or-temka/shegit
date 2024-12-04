1431b8665e9ba877fade1ba81d75276f68f28484
artem schegortzov <tema.chegortzov@mail.ru>
2024-12-04 11:09:10 +0300

    Add JSDoc

--- src/functions/noop/index.ts
+++ src/functions/noop/index.ts
@@ -1,3 +1,16 @@
+/**
+ * #### Noop function
+ *
+ * only return `undefined`. nothing is done inside.
+ *
+ * @since 1.1.0
+ * @category Helper
+ * @returns {undefined} `undefined`
+ * @example
+ *
+ * rgbObjToArray() // undefined
+ * rgbObjToArray("some") // undefined
+ */
 export function noop(...args: any[]): undefined {
   return undefined;
 }
