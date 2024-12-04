dcbaa08b095cb89a401e4f94fbe3eb487f86b7f8
artem schegortzov <tema.chegortzov@mail.ru>
2024-12-04 10:42:23 +0300

    Added JSDoc for function

--- src/functions/rgbObjToArray/index.ts
+++ src/functions/rgbObjToArray/index.ts
@@ -6,6 +6,21 @@ function roundDownAbsolute(value: number): number {
   return Math.floor(Math.abs(value));
 }
 
+/**
+ * #### RGB color object to RGB array
+ *
+ * Converts RGB color {r: [red], g: [green], b: [blue]} as an object
+ * to a format array [red, green, blue].
+ *
+ * @since 1.1.0
+ * @category Color
+ * @param {RgbObjToArrayInput} obj RGB color object
+ * @returns {RgbArray} RGB color array
+ * @example
+ *
+ * rgbObjToArray({r: 1, g: 2, b: 3}) // [1, 2, 3]
+ * rgbObjToArray({r: 400, g: () => "+54", b: -20, a: 1}) // [255, 54, 0]
+ */
 export function rgbObjToArray(obj: RgbObjToArrayInput = DEFAULT.input): RgbArray {
   let actual = DEFAULT.input;
   const isObject = Object.prototype.toString.call(obj) === '[object Object]';
