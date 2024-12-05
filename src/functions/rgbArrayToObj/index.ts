25accd63aed2c891e25bdee8ff835ecac7d4c52c
Or-temka <69795850+or-temka@users.noreply.github.com>
2024-12-05 21:40:56 +0300

    Updated JSDoc for functions (#97)
    For: noop, rgbArrayToObj, toColor

--- src/functions/rgbArrayToObj/index.ts
+++ src/functions/rgbArrayToObj/index.ts
@@ -12,14 +12,14 @@ function roundDownAbsolute(value: number): number {
  * Converts RGB color [red, green, blue] as an array
  * to a format object {r: [red], g: [green], b: [blue]}.
  *
- * @since 1.0.1
+ * @since 1.0.0
  * @category Color
  * @param {Input} array RGB color array
  * @returns {RgbObj} RGB color object
  * @example
  *
- * rgbArrayToObj([1, 2, 3])
- * // => {r: 1, g: 2, b: 3}
+ * rgbArrayToObj([1, 2, 3]) // {r: 1, g: 2, b: 3}
+ * rgbArrayToObj(["+44", -200, () => 12, 2]) // {r: 44, g: 0, b: 12}
  */
 export function rgbArrayToObj(array: Input = DEFAULT.input): RgbObj {
   let actual = DEFAULT.input;
--- src/functions/toColor/index.ts
+++ src/functions/toColor/index.ts
@@ -8,15 +8,15 @@ import { ColorProp, ReturnColorType, To } from './types';
  *
  * Convert a color of one type to another.
  *
- * @since 1.0.1
+ * @since 1.0.0
  * @category Color
  * @param {Color} color color in any representation
  * @param {To} to type of the return color
  * @returns {ReturnColorType<To>} the color in the selected "To" type
  * @example
  *
- * toColor([1, 2, 3], "object")
- * // => {r: 1, g: 2, b: 3, a: 1}
+ * toColor([1, 2, 3], "object") // {r: 1, g: 2, b: 3, a: 1}
+ * toColor(["+44", -200, () => 12, 2], "object") // {r: 44, g: 0, b: 12, a: 1}
  */
 export function toColor<T extends To>(
   color: ColorProp = DEFAULT.input,
