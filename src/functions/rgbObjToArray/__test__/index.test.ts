a22fe10e267dd0bcfaf168f07e0ae2fe0c287f94
artem schegortzov <tema.chegortzov@mail.ru>
2024-12-08 15:38:03 +0300

    Updated tests

--- src/functions/rgbObjToArray/__test__/index.test.ts
+++ src/functions/rgbObjToArray/__test__/index.test.ts
@@ -1,16 +1,13 @@
 import { rgbObjToArray } from '../../../../dist/functions/rgbObjToArray';
 import { describeTestGroups } from '../../../../utils';
 import { TestGroups } from '../../../../utils/test/describeTestGroups/types';
-import { extraElements, invalidTypes, mixedCases, partialArrays, validCases } from './groups';
+import { invalidCases, validCases } from './groups';
 import { TestGroupCaseElementExpected, TestGroupCaseElementInput } from './types';
 
 describe('RGB object to array', () => {
   const testGroups: TestGroups<TestGroupCaseElementInput, TestGroupCaseElementExpected> = [
     { name: 'Valid cases', cases: validCases },
-    { name: 'Partial arrays', cases: partialArrays },
-    { name: 'Extra elements', cases: extraElements },
-    { name: 'Invalid types', cases: invalidTypes },
-    { name: 'Mixed cases', cases: mixedCases },
+    { name: 'Invalid cases', cases: invalidCases },
   ];
 
   describeTestGroups({
