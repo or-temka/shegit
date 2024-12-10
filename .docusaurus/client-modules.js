3b79f8ae0e1ffe170a8537ecdcb184082a1cf1f7
artem schegortzov <tema.chegortzov@mail.ru>
2024-12-10 12:12:14 +0300

    Deleted docusaurus

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("C:\\Users\\temac\\Desktop\\shegit\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("C:\\Users\\temac\\Desktop\\shegit\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("C:\\Users\\temac\\Desktop\\shegit\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("C:\\Users\\temac\\Desktop\\shegit\\src\\css\\custom.css"),
-];
