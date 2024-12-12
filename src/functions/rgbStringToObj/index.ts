21811ced2bd9e327ebe0d53d9a27cf4082faeb22
artem schegortzov <tema.chegortzov@mail.ru>
2024-12-12 18:24:44 +0300

    Added JSDoc

--- src/functions/rgbStringToObj/index.ts
+++ src/functions/rgbStringToObj/index.ts
@@ -18,6 +18,24 @@ const parseAlphaComponent = (alphaComponent: string): number => {
   return num < 0 ? 0 : num > 1 ? 1 : num;
 };
 
+/**
+ * #### RGB color string to object
+ *
+ * Converts RGB color `rgb(red, green, blue)` as an string
+ * to a format object `{r: red, g: green, b: blue, a: alpha}`.
+ *
+ * @since 1.1.0
+ * @category Color
+ * @param {RgbStringToObjInput} string RGB color string
+ * @returns {RgbaObj} RGB color object
+ * @example
+ *
+ * rgbStringToObj("rgb(1, 2, 3)")
+ * // {r: 1, g: 2, b: 3, a: 1}
+ *
+ * rgbStringToObj(() => "rgba(500, 12, -200, 0.5)")
+ * // {r: 255, g: 12, b: 0, a: 0.5}
+ */
 export function rgbStringToObj(string: RgbStringToObjInput = DEFAULT.input): RgbaObj {
   let actual = string;
 
