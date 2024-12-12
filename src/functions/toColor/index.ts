9bb8d292257405b73a48086c5895fb4adc85a516
artem schegortzov <tema.chegortzov@mail.ru>
2024-12-12 21:29:10 +0300

    added line support & some fix with object

--- src/functions/toColor/index.ts
+++ src/functions/toColor/index.ts
@@ -1,15 +1,29 @@
+import { rgbStringToObj } from '../rgbStringToObj';
+import { rgbObjToString } from '../rgbObjToString';
 import { rgbObjToArray } from '../rgbObjToArray';
 import { rgbArrayToObj } from '../rgbArrayToObj';
 import { RgbaObj } from '../../types';
 import { DEFAULT, DEFAULT_COLOR_COMPONENT } from './constants';
 import { ColorProp, ReturnColorType, To } from './types';
+import { absoluteFloor } from '../absoluteFloor';
+
+const parseComponent = (component: any): number => {
+  let actual = component;
+  if (typeof component === 'function') actual = component();
+  const num = Number(actual);
+  return Number.isNaN(num) || num < 0
+    ? DEFAULT_COLOR_COMPONENT
+    : num > 255
+    ? 255
+    : absoluteFloor(num) || DEFAULT_COLOR_COMPONENT;
+};
 
 /**
  * #### To color
  *
  * Convert a color of one type to another.
  *
- * @since 1.0.0
+ * @since 1.0.1
  * @category Color
  * @param {Color} color color in any representation
  * @param {To} to type of the return color
@@ -38,14 +52,15 @@ export function toColor<T extends To>(
       Object.assign(c, { r, g, b, a: 1 });
     } else {
       if (color !== null) {
-        const {
-          r = DEFAULT_COLOR_COMPONENT,
-          g = DEFAULT_COLOR_COMPONENT,
-          b = DEFAULT_COLOR_COMPONENT,
-        } = color;
+        const r = parseComponent(color.r || DEFAULT_COLOR_COMPONENT);
+        const g = parseComponent(color.g || DEFAULT_COLOR_COMPONENT);
+        const b = parseComponent(color.b || DEFAULT_COLOR_COMPONENT);
         Object.assign(c, { r, g, b, a: 1 });
       }
     }
+  } else if (typeof color === 'string') {
+    const { r, g, b, a = 1 } = rgbStringToObj(color);
+    Object.assign(c, { r, g, b, a });
   }
 
   switch (to) {
@@ -54,6 +69,8 @@ export function toColor<T extends To>(
       return c as ReturnColorType<T>;
     case 'rgb-array':
       return rgbObjToArray(c) as ReturnColorType<T>;
+    case 'rgb-string':
+      return rgbObjToString(c) as ReturnColorType<T>;
     default:
       return c as ReturnColorType<T>;
   }
