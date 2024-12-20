e8eb99ee166ae29f4a8e6c28a67ecbf71f5b5ed7
artem schegortzov <tema.chegortzov@mail.ru>
2024-12-20 21:12:42 +0300

    Done tests

---
+++ src/functions/colors/utility/cmykObjToArray/__test__/index.test.ts
@@ -0,0 +1,17 @@
+import { cmykObjToArray } from '../../../../../../dist';
+import { describeTestGroups } from '../../../../../../utils';
+import { TestGroups } from '../../../../../../utils/test/describeTestGroups/types';
+import { invalidCases, validCases } from './groups';
+import { TestGroupCaseElementExpected, TestGroupCaseElementInput } from './types';
+
+describe('CMYK object to array', () => {
+  const testGroups: TestGroups<TestGroupCaseElementInput, TestGroupCaseElementExpected> = [
+    { name: 'Valid cases', cases: validCases },
+    { name: 'Invalid cases', cases: invalidCases },
+  ];
+
+  describeTestGroups({
+    testGroups,
+    callback: (input) => cmykObjToArray(input),
+  });
+});
