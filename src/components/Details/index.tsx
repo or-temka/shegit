c145e1facbd6b3dcea16446918780b62bbe5784b
Or_temka <tema.chegortzov@mail.ru>
2025-01-06 21:35:25 +0300

    Updated Details component

--- src/components/Details/index.tsx
+++ src/components/Details/index.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import styles from './style.module.css';
 import { DetailsProps } from './types';
 
-function Details({ children, headerText = 'headerText' }: DetailsProps) {
+function Details({ children, headerText = 'headerText', style }: DetailsProps) {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleDetails = () => {
@@ -10,12 +10,21 @@ function Details({ children, headerText = 'headerText' }: DetailsProps) {
   };
 
   return (
-    <div className={styles.details}>
-      <button className={styles.button} onClick={toggleDetails}>
-        <div className={styles.rect} style={{ transform: `rotate(${isOpen ? 0 : 180}deg)` }}></div>
+    <div className={styles.details} style={style?.wrapper}>
+      <button className={styles.button} onClick={toggleDetails} style={style?.button}>
+        <div
+          className={styles.rect}
+          style={{ transform: `rotate(${isOpen ? 0 : 180}deg)`, ...style?.buttonIcon }}
+        ></div>
         {headerText}
       </button>
-      {isOpen ? <div className={styles.content}>{children}</div> : <></>}
+      {isOpen ? (
+        <div className={styles.content} style={style?.content}>
+          {children}
+        </div>
+      ) : (
+        <></>
+      )}
     </div>
   );
 }
--- src/components/Details/types.ts
+++ src/components/Details/types.ts
@@ -3,4 +3,10 @@ import { ReactNode } from 'react';
 export type DetailsProps = {
   children: ReactNode;
   headerText: string;
+  style?: {
+    wrapper?: React.CSSProperties;
+    button?: React.CSSProperties;
+    buttonIcon?: React.CSSProperties;
+    content?: React.CSSProperties;
+  };
 };
