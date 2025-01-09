cc72fed6da3e7294ff04bb48e1d1ae01599c52d3
Or-temka <69795850+or-temka@users.noreply.github.com>
2025-01-09 11:58:30 +0300

    Done `hexStringToRgbObj` func. (#151)
    * Done functions
    
    * Done tests
    
    * Added JSDoc examples for function

---
+++ src/functions/colors/utility/hexStringToRgbObj/__test__/index.test.ts
@@ -0,0 +1,17 @@
+import { hexStringToRgbObj } from '../../../../../../dist';
+import { describeTestGroups } from '../../../../../../utils';
+import { TestGroups } from '../../../../../../utils/test/describeTestGroups/types';
+import { invalidCases, validCases } from './groups';
+import { TestGroupCaseElementExpected, TestGroupCaseElementInput } from './types';
+
+describe('HEX string to RGB object', () => {
+  const testGroups: TestGroups<TestGroupCaseElementInput, TestGroupCaseElementExpected> = [
+    { name: 'Valid cases', cases: validCases },
+    { name: 'Invalid cases', cases: invalidCases },
+  ];
+
+  describeTestGroups({
+    testGroups,
+    callback: (input) => hexStringToRgbObj(input),
+  });
+});
