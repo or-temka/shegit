f4ea06523b6297217ab1d8c41614abcc3fe503da
Or-temka <69795850+or-temka@users.noreply.github.com>
2025-01-24 20:35:12 +0300

    Done `hsvObjToRgbObj` function (#167)
    * Done function
    
    * Done tests
    
    * Updated examples for JSDoc

---
+++ src/functions/colors/utility/hsvObjToRgbObj/__test__/index.test.ts
@@ -0,0 +1,17 @@
+import { hsvObjToRgbObj } from '../../../../../../dist';
+import { describeTestGroups } from '../../../../../../utils';
+import { TestGroups } from '../../../../../../utils/test/describeTestGroups/types';
+import { invalidCases, validCases } from './groups';
+import { TestGroupCaseElementExpected, TestGroupCaseElementInput } from './types';
+
+describe('HSV object to RGB object', () => {
+  const testGroups: TestGroups<TestGroupCaseElementInput, TestGroupCaseElementExpected> = [
+    { name: 'Valid cases', cases: validCases },
+    { name: 'Invalid cases', cases: invalidCases },
+  ];
+
+  describeTestGroups({
+    testGroups,
+    callback: (input) => hsvObjToRgbObj(input),
+  });
+});
