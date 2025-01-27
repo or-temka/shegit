c466348ea3f5286fef83c353313adfbb47694274
artem schegortzov <tema.chegortzov@mail.ru>
2025-01-27 03:13:47 +0300

    Updated tests for 'from hsv obj to rgb array & string'

--- src/functions/colors/toColor/__test__/groups/fromHsv/toRgb/index.ts
+++ src/functions/colors/toColor/__test__/groups/fromHsv/toRgb/index.ts
@@ -1,8 +1,9 @@
 import { TestGroups } from '../../../../../../../../utils/test/describeTestGroups/types';
 import { To } from '../../../../types';
 import { TestGroupCaseElementExpected, TestGroupCaseElementInput } from '../../../types';
-
 import { hsvObjectToRgbObjectCases } from './objectToObject';
+import { hsvObjectToRgbArrayCases } from './objectToArray';
+import { hsvObjectToRgbStringCases } from './objectToString';
 
 export const fromHsvToRgbGroups: TestGroups<
   TestGroupCaseElementInput,
@@ -13,6 +14,12 @@ export const fromHsvToRgbGroups: TestGroups<
   // from object
   { name: 'Valid: HSV object to RGB object', cases: hsvObjectToRgbObjectCases.valid },
   { name: 'Invalid: HSV object to RGB object', cases: hsvObjectToRgbObjectCases.invalid },
+
+  { name: 'Valid: HSV object to RGB array', cases: hsvObjectToRgbArrayCases.valid },
+  { name: 'Invalid: HSV object to RGB array', cases: hsvObjectToRgbArrayCases.invalid },
+
+  { name: 'Valid: HSV object to RGB string', cases: hsvObjectToRgbStringCases.valid },
+  { name: 'Invalid: HSV object to RGB string', cases: hsvObjectToRgbStringCases.invalid },
 
   // from string
 ];
