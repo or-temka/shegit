e5b3a8958a8e581f87bccc24513113011e7df3f7
Or-temka <69795850+or-temka@users.noreply.github.com>
2025-03-06 01:46:16 +0300

    Add default & exception errors for `live function preview` (#232)

--- src/components/LiveFunctionPreview/types.ts
+++ src/components/LiveFunctionPreview/types.ts
@@ -1,7 +1,16 @@
+/**
+ * - `name` - Function name
+ * - `optional` - Is the argument optional
+ * - `default` - Value that will be displayed when the input field is empty
+ * - `initial` - The value that will be displayed when a component first appears
+ * - `type` - Type of expected value. It is then converted to this type or `undefined`
+ */
 export type Argument = {
   name: string;
   optional?: boolean;
+  initial?: string;
   default?: string;
+  type?: 'number';
 };
 
 export type LiveFunctionPreviewProps = {
