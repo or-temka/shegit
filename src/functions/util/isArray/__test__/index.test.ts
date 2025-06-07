dda54cced092935e4ff831339b55e782ed7fd669
Artem Shchegortzov <69795850+or-temka@users.noreply.github.com>
2025-06-07 14:20:40 +0300

    Add `isArray` func. (#265)
    * Done
    
    * Add in changelog

---
+++ src/functions/util/isArray/__test__/index.test.ts
@@ -0,0 +1,16 @@
+import { isArray } from '../../../../../dist';
+import { describeTestGroups } from '../../../../../utils';
+import { TestGroups } from '../../../../../utils/test/describeTestGroups/types';
+import { validCases } from './groups';
+import { TestGroupCaseElementExpected, TestGroupCaseElementInput } from './types';
+
+describe('isArray', () => {
+  const testGroups: TestGroups<TestGroupCaseElementInput, TestGroupCaseElementExpected> = [
+    { name: 'Valid cases', cases: validCases },
+  ];
+
+  describeTestGroups({
+    testGroups,
+    callback: (input) => isArray(input),
+  });
+});
