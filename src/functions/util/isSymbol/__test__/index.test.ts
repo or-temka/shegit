536bf060e19633e8bb0765738fffc54dea798884
artem schegortzov <tema.chegortzov@mail.ru>
2025-06-09 19:20:05 +0300

    Done

---
+++ src/functions/util/isSymbol/__test__/index.test.ts
@@ -0,0 +1,16 @@
+import { isSymbol } from '../../../../../dist';
+import { describeTestGroups } from '../../../../../utils';
+import { TestGroups } from '../../../../../utils/test/describeTestGroups/types';
+import { validCases } from './groups';
+import { TestGroupCaseElementExpected, TestGroupCaseElementInput } from './types';
+
+describe('isSymbol', () => {
+  const testGroups: TestGroups<TestGroupCaseElementInput, TestGroupCaseElementExpected> = [
+    { name: 'Valid cases', cases: validCases },
+  ];
+
+  describeTestGroups({
+    testGroups,
+    callback: (input) => isSymbol(input),
+  });
+});
