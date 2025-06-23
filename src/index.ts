c9a8df23b76431fa5037582b9a9fe30129761734
artem schegortzov <tema.chegortzov@mail.ru>
2025-06-23 22:50:03 +0300

    Bump to version 1.3.0

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
-import { rgbArrayToObj, toColor } from 'shegit';
+import _ from 'shegit';
 import exec from './utils/exec.js';
 
+console.log(`VERSION: ${_.VERSION}`);
+
 const input = [1, 2, 3];
-exec(rgbArrayToObj, [input]);
-exec(toColor, [input]);
+exec(_.rgbArrayToObj, [input]);
+exec(_.toColor, [input]);
+
+exec(_.mergeObjects, [
+  { a: 3, b: 4 },
+  { a: 2, c: 5 },
+]);
