7bb0f2c93df2d3dfd61922df4bfd3ef6f0f4cba5
Artem Shchegortzov <69795850+or-temka@users.noreply.github.com>
2025-06-26 22:34:22 +0300

    Add code sandbox for `number` category functions (#294)

--- docs/number/constants.ts
+++ docs/number/constants.ts
@@ -11,5 +11,6 @@ export const FUNCTIONS_CONFIG: FunctionsConfig<FunctionNames> = {
       available: '1.3.0',
     },
     resourceLinkPath: `${RESOLVE_LINK_PATH_CATEGORY}absoluteFloor`,
+    codeSandboxLink: 'https://codesandbox.io/p/sandbox/absolutefloor-zm9c94',
   },
 };
